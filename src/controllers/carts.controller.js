2fef395b61521d1f2dd436c7a62d8cff91b58d17
Brando perez <brandoperezinciarte@gmail.com>
2023-11-25 18:53:10 -0300

    Tercera práctica integradora, cambios finales

--- src/controllers/carts.controller.js
+++ src/controllers/carts.controller.js
@@ -8,8 +8,31 @@ export const createCart = async (req, res) => {
         const respuesta = await cartsModel.create({});
         res.status(200).send({ resultado: "OK", message: respuesta });
       } catch (error) {
-        res.status(500).send({ error: `Error al crear producto: ${error}` });
+        res.status(400).send({ error: `Error al crear el carrito: ${error}` });
       }
+}
+
+export const addProductToCart = async (req, res) =>{
+
+    const {cid, pid} = req.params;
+    try{
+        const cartId = cid;
+        const cart = await cartsModel.findById(cartId)
+            if(cart){
+                const productoExistente = cart.products.find(product => product.product === pid);
+                    if(productoExistente){
+                        productoExistente.quantity++
+                        }else{
+                            cart.products.push({ product: pid, quantity: 1})
+                        }
+
+            res.status(200).send({ respuesta: 'OK', message: cart})
+            }else{
+            res.status(404).send({ respuesta: 'Not found', message: 'No se pudo encontrar la tarjeta'});
+            }
+    }catch(error){
+        res.status(400).send({ error: `Error al cargar la tarjeta: ${error}`});
+    }
 }
 
 export const updateProductToCart = async (req, res) => { 
@@ -36,14 +59,32 @@ export const updateProductToCart = async (req, res) => {
         }
 }
 
+export const updateCart = async (req, res) =>{
+    
+    const { cid } = req.params;
+    const { products } = req.body;
+
+        try{
+            const cart = await cartsModel.findById(cid);
+            if(!cart){
+                res.status(404).send({ resultado: 'Not found', message: 'Producto no encontrado'});
+            }
+            cart.products = products;
+
+            await cart.save();
+            res.status(200).send({ resultado: 'OK', message: "Producto actualizado correctamente"});
+        }catch(error){
+            res.status(400).send({ error: `Error al actualizar el producto del carrito ${error}`});
+        }
+}
 export const getCartById = async (req, res) => {
     
     const { cid } = req.params;
     try{
         const cart = await cartsModel.findById(cid).populate('products.product');
-        res.status(200).send({ respuesta: 'OK', message: prods});
+        cart ? res.status(200).send({ respuesta: 'OK', message: prods}) : res.status(404).send({error: 'No se encontró el carrito'});
     }catch(error){
-        res.status(400).send({ error: `Error al encontrar el id del carrito ${error}`})
+        res.status(500).send({ error: `Error al encontrar el id del carrito ${error}`})
     }
 }
 
@@ -82,69 +123,36 @@ export const deleteCart = async (req, res) =>{
     }
 }
 
-export const addProductToCart = async (req, res) =>{
-
-    const {cid, pid} = req.params;
-    try{
-        const cartId = cid;
-        const cart = await cartsModel.findById(cartId)
-            if(cart){
-                const productoExistente = cart.products.find(product => product.product === pid);
-                    if(productoExistente){
-                        productoExistente.quantity++
-                        }else{
-                            cart.products.push({ product: pid, quantity: 1})
-                        }
-
-            res.status(200).send({ respuesta: 'OK', message: cart})
-            }else{
-            res.status(404).send({ respuesta: 'Not found', message: 'No se pudo encontrar la tarjeta'});
-            }
-    }catch(error){
-        res.status(400).send({ error: `Error al cargar la tarjeta: ${error}`});
-    }
-}
-
-export const updateCart = async (req, res) =>{
-    
-    const { cid } = req.params;
-    const { products } = req.body;
-
-        try{
-            const cart = await cartsModel.findById(cid);
-            if(!cart){
-                res.status(404).send({ resultado: 'Not found', message: 'Producto no encontrado'});
-            }
-            cart.products = products;
-
-            await cart.save();
-            res.status(200).send({ resultado: 'OK', message: "Producto actualizado correctamente"});
-        }catch(error){
-            res.status(400).send({ error: `Error al actualizar el producto del carrito ${error}`});
-        }
-}
-
 export const purcharseCart = async (req, res) => {
 
     const { cid } = req.params;
         try{
             const cart = await cartsModel.findById(cid);
             const products = await productsModel.find();
+
                 if(cart){
-                    const user = await usersModel.find({ cart: cart._id});
-                    const email = user[0].email;
+                    const user = await userModel.findOne({ cart: cart._id});
+                    const email = user.email;
                     let amount = 0;
                     const itemsCompra = [];
-                    cart.products.forEach( async item => {
-                        const product = products.find(prod => prod._id == item.id_prod.toString());
-                            if(product.stock >= item.quantity){
-                                amount += product.price * item.quantity;
-                                product.stock -= item.quantity;
-                                await product.save();
-                                itemsCompra.push(product.title);
+                    for(const item of cart.products){
+                        const product = products.find(prod => prod._id.toString() === item.id_prod.toString());
+                            if(product && product.stock >= item.quantity){
+                                const price = product.price;
+                                const quantity = item.quantity;
+                                
+                                    if(userModel.role === 'premiun'){
+                                        amount += price * quantity * 0.8;
+                                    }else{
+                                        amount += price * quantity;
+                                    }
+
+                                    product.stock -= quantity;
+                                    await product.save();
+                                    itemsCompra.push(product.title);
                             }
-                    });
-                    console.log(itemsCompra);
+                    }
+                                    
                     await cartsModel.findByIdAndUpdate(cid, { products: [] });
                     res.redirect(`http://localhost:4000/api/tickets/create?amount=${amount}&email=${email}`);
                 }else{
@@ -152,5 +160,18 @@ export const purcharseCart = async (req, res) => {
                 }
         }catch(error){
             res.status(500).send({ error: `Error al cargar el carrito: ${error}`});
+        }
+}
+
+export const getCarts = async (req, res) =>{
+    const limit = req.query;
+        try{
+            const carts = await cartsModel.find({}).limit(parseInt(limit));
+            res.status(carts && carts.length > 0 ? 200 : 404).send({
+                resultado: carts && carts.length > 0 ? 'ok' : 'Not fount',
+                message: carts && carts.length > 0 ? carts : 'No se encontraron carritos'
+            })
+        }catch(error){
+            res.status(400).send({ error: `Error al obtener carritos: ${error}` });
         }
 }
